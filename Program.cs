687e42948e0132687fae5f0bdf1fd000cac331ed
bahtitm <bahti1986@gmail.com>
2022-01-19 20:50:07 +0500

    Added logging

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace TodoApi
 {
@@ -18,9 +13,12 @@ namespace TodoApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+               .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+            .UseSerilog((hostingContext, loggerConfig) =>
+            loggerConfig.ReadFrom.Configuration(hostingContext.Configuration)
+        );
     }
 }
